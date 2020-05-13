06673d23a1e6041778799e29e4b81bf5c3ae01a7
Mike Decker <pookmish@gmail.com>
2020-05-13 15:39:16 -0700

    log in for codeception test

--- tests/codeception/acceptance/StanfordPersonCest.php
+++ tests/codeception/acceptance/StanfordPersonCest.php
@@ -3,6 +3,7 @@
 class StanfordPersonCest {
 
   public function testPersonNode(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
     $I->amOnPage('/node/add/stanford_person');
     $I->canSee('First Name');
   }
