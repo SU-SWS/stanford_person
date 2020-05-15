3ab8d6efdf6e7c39f74f2f9a39379eb3014521bf
Sherakama <sherakama@gmail.com>
2020-05-15 09:03:02 -0700

    D8CORE-1969: Codeception Tests For Person (#88)

--- tests/codeception/acceptance/StanfordPersonCest.php
+++ tests/codeception/acceptance/StanfordPersonCest.php
@@ -1,11 +1,63 @@
 <?php
 
 class StanfordPersonCest {
-
+  
+  /**
+   * People
+   */
   public function testPersonNode(AcceptanceTester $I) {
     $I->logInWithRole('administrator');
     $I->amOnPage('/node/add/stanford_person');
     $I->canSee('First Name');
+  }
+
+  /**
+   * Test that the vocabulary and terms exist.
+   */
+  public function testVocabularyTermsExists(AcceptanceTester $I) {
+    $I->logInWithRole('administrator');
+    $I->amOnPage("/admin/structure/taxonomy/manage/stanford_person_types/overview");
+    $I->canSeeResponseCodeIs(200);
+  }
+
+  /**
+   * Test that the view pages exist.
+   */
+  public function testViewPagesExist(AcceptanceTester $I) {
+    $I->createEntity([
+      'vid' => 'stanford_person_types',
+      'name' => "Student",
+      'description' => "Student",
+    ], 'taxonomy_term');    
+    $I->createEntity([
+      'vid' => 'stanford_person_types',
+      'name' => "Staff",
+      'description' => "Staff",
+    ], 'taxonomy_term');
+    $I->amOnPage("/people");
+    $I->canSeeResponseCodeIs(200);
+    $I->see("Sorry, no results found");
+    $I->amOnPage("/people/staff");
+    $I->canSeeResponseCodeIs(200);
+    $I->see("Sorry, no results found");
+    $I->see("Filter By Person Type");
+  }
+
+  /**
+   * Test that content that gets created has the right url, header, and shows
+   * up in the all view.
+   */
+  public function testCreatePerson(AcceptanceTester $I) {
+    $I->createEntity([
+      'type' => 'stanford_person',
+      'su_person_first_name' => "John",
+      'su_person_last_name' => "Wick",
+    ]);
+    $I->amOnPage("/person/john-wick");
+    $I->see("John Wick");
+    $I->runDrush('cr');
+    $I->amOnPage("/people");
+    $I->see("John Wick");
   }
 
 }
