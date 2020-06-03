06e06c40b3421be95dcfd0f01acbd3a7db18956a
Mike Decker <pookmish@gmail.com>
2020-06-03 12:41:32 -0700

    create term manually

--- tests/codeception/acceptance/StanfordPersonImporterCest.php
+++ tests/codeception/acceptance/StanfordPersonImporterCest.php
@@ -12,16 +12,15 @@ class StanfordPersonImporterCest {
     $I->runDrush('pm:enable stanford_person_importer');
     $I->runDrush('cr');
     $I->logInWithRole('administrator');
+    $I->amOnPage('/admin/structure/taxonomy/manage/cap_org_codes/add');
+    $I->fillField('Name', 'Web Services');
+    $I->fillField('su_cap_org_code[0][value]', 'BSWS');
+
     $I->amOnPage('/admin/config/people/person-importer');
     $I->fillField('CAP Username', getenv('CAP_USERNAME'));
     $I->fillField('CAP Password', getenv('CAP_PASSWORD'));
     $I->click('Save');
 
-    $I->createEntity([
-      'vid' => 'cap_org_codes',
-      'name' => 'Web Services',
-      'su_cap_org_code' => 'BSWS',
-    ], 'taxonomy_term');
     $I->fillField('su_person_orgs[0][target_id]', 'Web Services');
     $I->fillField('su_person_workgroup[0][value]', 'uit:sws');
     $I->click('Save');
