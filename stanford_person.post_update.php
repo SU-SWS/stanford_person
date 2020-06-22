0d44d0970646f9b1a46f927b5c95450ea4d0b525
Shea McKinney <sherakama@gmail.com>
2020-06-22 16:26:17 -0700

    fixup! added install/update changes.

--- stanford_person.post_update.php
+++ stanford_person.post_update.php
@@ -8,6 +8,8 @@
  */
 
 use \Drupal\Core\File\FileSystemInterface;
+use Drupal\file\Entity\File;
+use Drupal\media\Entity\Media;
 
 /**
  * Install the default profile image to the media library and add the entities
@@ -16,50 +18,37 @@ use \Drupal\Core\File\FileSystemInterface;
 function stanford_person_post_update_8100() {
 
   // Save the file to public.
-  $source = drupal_get_path("module", "stanford_person") . "/lib/assets/img/default-profile-image.png";
+  $source = drupal_get_path("module", "stanford_person") . "/lib/assets/img/stanford-person-default-profile-image.png";
+  $destination = 'public://media/image';
   $file_system = \Drupal::service('file_system');
-  $file_system->copy($source, 'public://media/image', FileSystemInterface::EXISTS_REPLACE);
+  $saved = $file_system->copy($source, $destination, FileSystemInterface::EXISTS_REPLACE);
 
-  // Save the file content entity.
-  try {
-    _stanford_person_import_default_content("file/4d5dde7e-f2c7-4d27-8ad1-99623b1308f8.json");
-  }
-  catch(\Exception $e) {
-    \Drupal::logger('stanford_person')->error($e->message());
-  }
+  // Save the file/media entities and ensure their UUIDs.
+  // Create file entity.
+  $image = File::create();
+  $image->setFileUri($saved);
+  $image->set('uuid', '4d5dde7e-f2c7-4d27-8ad1-99623b1308f8');
+  $image->setOwnerId(\Drupal::currentUser()->id());
+  $image->setMimeType('image/' . pathinfo($saved, PATHINFO_EXTENSION));
+  $image->setFileName($file_system->basename($saved));
+  $image->setPermanent();
+  $image->save();
 
-  // Save the media content entity.
-  try {
-    _stanford_person_import_default_content("media/a4660e7f-d4bf-4a28-8030-9dc8576b1c9a.json");
-  }
-  catch(\Exception $e) {
-    \Drupal::logger('stanford_person')->error($e->message());
-  }
+  // Create media entity with saved file.
+  $image_media = Media::create([
+    'bundle' => 'image',
+    'uuid' => 'a4660e7f-d4bf-4a28-8030-9dc8576b1c9a',
+    'uid' => \Drupal::currentUser()->id(),
+    'langcode' => \Drupal::languageManager()->getDefaultLanguage()->getId(),
+    'status' => Media::PUBLISHED,
+    'field_media_image' => [
+      'target_id' => $image->id(),
+      'alt' => t('Placeholder image'),
+      'title' => t('Placeholder image'),
+    ],
+  ]);
+  $image_media->save();
 
-}
-
-/**
- * Import a piece of content exported by default content module.
- *
- * Shamelessly borrowed from: https://www.drupal.org/project/default_content/issues/2803005
- *
- * @param string $path_to_content_json
- *   The path to the json file.
- */
-function _stanford_person_import_default_content($path_to_content_json) {
-  global $base_url;
-  list($entity_type_id, $filename) = explode('/', $path_to_content_json);
-  $p = drupal_get_path('module', 'stanford_person');
-  $encoded_content = file_get_contents($p . '/content/' . $path_to_content_json);
-  $serializer = \Drupal::service('serializer');
-  $content = $serializer->decode($encoded_content, 'hal_json');
-  $url = $base_url . base_path();
-  $content['_links']['type']['href'] = str_replace('http://drupal.org/', $url, $content['_links']['type']['href']);
-  $contents = $serializer->encode($content, 'hal_json');
-  $class = 'Drupal\\' . $entity_type_id . '\Entity\\' . str_replace(' ', '', ucwords(str_replace('_', ' ', $entity_type_id)));
-  $entity = $serializer->deserialize($contents, $class, 'hal_json', array('request_method' => 'POST'));
-  $entity->enforceIsNew(TRUE);
-  $entity->save();
 }
 
 
