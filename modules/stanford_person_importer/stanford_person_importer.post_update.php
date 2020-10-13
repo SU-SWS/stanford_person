5a976773548b11d54b32c2d0f25a318958276726
pookmish <pookmish@stanford.edu>
2020-10-13 13:49:08 -0700

    D8CORE-2470 Use the default image on any imported content without an image (#118)

---
+++ modules/stanford_person_importer/stanford_person_importer.post_update.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @file
+ * stanford_person_importer.post_update.php
+ */
+
+/**
+ * Invalidate all migration profiles that don't have a photo.
+ */
+function stanford_person_importer_post_update_8001(&$sandbox) {
+  if (!\Drupal::database()
+    ->schema()
+    ->tableExists('migrate_map_su_stanford_person')) {
+    return;
+  }
+
+  $nids = \Drupal::entityTypeManager()->getStorage('node')
+    ->getQuery()
+    ->accessCheck(FALSE)
+    ->condition('type', 'stanford_person')
+    ->condition('su_person_photo', NULL, 'IS NULL')
+    ->execute();
+
+  \Drupal::database()->update('migrate_map_su_stanford_person')
+    ->fields(['hash' => ''])
+    ->condition('destid1', array_values($nids), 'IN')
+    ->execute();
+}
